4c875676e17922d9500154d929b3e75a8f71a818
Matthew Melachrinos <matthewmelachrinosw@Matthews-iMac.local>
2016-02-02 17:50:28 -0500

    added input file

--- 2HW.cpp
+++ 2HW.cpp
@@ -2,12 +2,27 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <stdlib.h>
 using namespace std;
 
 
 bool miller_rabin(int data){
   bool isPrime;
-  isPrime = true;
+  int k = 1;
+  float m;
+  while (1){
+    m = (data-1)/(pow(2,k));
+    if (m != (int)m){
+      k--;
+      m = (data-1)/(pow(2,k));
+      break;
+    }
+
+    k++;
+  }
+  a = rand() % (data-2) + 2;
+
+
   return isPrime;
 }
 
@@ -17,7 +32,6 @@ int main(){
   infile.open("hw2.dat", ios::in);
   int data;
   for (; !infile.eof() ; infile >> data){
-
     cout << data << " ";
     if(miller_rabin(data)){
       cout << "true" << endl;
