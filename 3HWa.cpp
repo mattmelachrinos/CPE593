14ae94cdb66c44d619ceccb407930ae2b321b432
Matthew Melachrinos <mmelach@gmail.com>
2016-02-21 23:30:43 -0500

    implented binary search algorithm. Some bugs remain

--- 3HWa.cpp
+++ 3HWa.cpp
@@ -17,8 +17,33 @@ Worked closely with Dillon Guarino
 #include <string>
 using namespace std;
 
-int search(int a[], int key ){
+int search(int a[], int key , int size){
+	int start = 0;
+	int end = size-1;
+	int guess;
+	int number = 0;
+	cout << " " << endl;
+	if (key < a[end] && key > a[start]){
+		while (a[guess] != key){
+			number++;
+			guess = (start + end)/2;
+			if (a[guess] > key){
+				end = guess;
+				guess = (start + end)/2;
+			}
+			else if ( a[guess] < key){
+				start = guess;
+				guess = (start + end)/2;
+			}
 
+
+			if (guess <= start || guess >= end){
+				number = -number;
+				break;
+			}
+		}
+	}
+	return number;
 }
 
 int main(){
@@ -31,17 +56,21 @@ int main(){
     int result;
     string first_word;
     infile >> first_word;
-    while (!.eof){
+		infile >> first_word;
+    while (!infile.eof()){
 		  infile >> n;
 		  int a[n];
 		  for (int i=0 ; i<n ; i++){
-			   infile >> a[i];
+		  	infile >> a[i];
 		  }
       infile >> key;
-      result = search(a,key);
-      if (result == -1)
-
-      delete a[];
+      result = search(a,key,n);
+      if (result < 0 )
+				cout << "Not Found : " << abs(result) << endl;
+			else if (result > 0 )
+				cout << "Found : " << result << endl;
+			else
+				cout << "ERROR" << endl;
     }
 		infile.close();
 	}
