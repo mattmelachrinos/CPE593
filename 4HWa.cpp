0aa43c97c43116206508a857d63e2929bba9b570
Matthew Melachrinos <mmelach@gmail.com>
2016-03-04 02:09:06 -0500

    fixed a lot of the file reading code. Still need to get switch statement working and add/remove front

--- 4HWa.cpp
+++ 4HWa.cpp
@@ -7,6 +7,7 @@ Worked closely with Dillon Guarino
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <math.h>
 #include <stdlib.h>
 #include <string>
@@ -24,7 +25,7 @@ class GrowArray {
       int* temp = new int[size];
       copy(p, p+size, temp);
       p = temp;
-      temp = null;
+      temp = nullptr;
       delete [] temp;
   	}
 
@@ -44,6 +45,9 @@ class GrowArray {
         grow();
       for (int i = 0 ; i < n ; i++)
         p[i+n-1] = v[i];
+    }
+    void addStart(int v [] , int n){
+
     }
     void removeFront(int n[]){
 
@@ -81,20 +85,27 @@ int main(){
 
 	ifstream infile;
 	infile.open("HW4a.txt", ios::in);
+  string instruction, line;
 
 	if (infile.is_open()){
 
     while (getline(infile,line)){
-      string instruction;
-      instruction = infile.getline();
+
       istringstream iss(line);
       istringstream checkNum(line);
       iss >> instruction;
       checkNum >> instruction;
+      int n = 0;
+      while (checkNum >> n)
+        n++;
       int values [n];
+      for (int i = 0 ; i < n ; i++){
+        iss >> values[i];
+      }
+      enum instructionType {ADD_FRONT, ADD_BACK, REMOVE_FRONT, REMOVE_BACK, OUTPUT};
       switch (instruction){
-        case ADD_FRONT: a.addStart(values);
-        case ADD_BACK: a.addEnd(values);
+        case ADD_FRONT: a.addStart(values,n);
+        case ADD_BACK: a.addEnd(values,n);
         case REMOVE_FRONT: a.removeFront(values);
         case REMOVE_BACK: a.removeBack(values);
         case OUTPUT: a.printArray();
